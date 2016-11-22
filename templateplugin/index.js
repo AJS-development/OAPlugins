abf1ff263fb39a74ca92a20ebfd3220f0f98c7a4
Andrew S <andrews54757@gmail.com>
2016-11-21 21:32:19 -0500

    Create index.js

---
+++ templateplugin/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+// [General]
+name: "", // Name of plugin REQUIRED
+author: "", // author REQUIRED
+description: '', // Desciprtion
+minVersion: '', // minimum version requirements (optional)
+version:  '', // version REQUIRED
+addToHelp: false, // add to help
+ commands: false, // commands
+ configFile: false, //config file
+// [Functions]
+init: function (data,configs) {
+  // init, Used to do stuff such as overriding things
+}
+}
