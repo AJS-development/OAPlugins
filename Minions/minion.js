c3108140b3d1a5c6360674061564a36a7c9b03a3
Andrew S <andrews54757@gmail.com>
2016-11-23 14:43:43 -0500

    Create minion.js

---
+++ Minions/minion.js
@@ -0,0 +1,32 @@
+module.exports = function(str,main,log) {
+         str = str.split(" ")
+         if (str[1] == "destroy") {
+             main.minions.forEach(function(minion){
+                 main.removeMinion(minion)
+             })
+             
+             return log("Removed all minions")
+         }
+             var id = parseInt(str[1])
+         if (isNaN(id)) {
+             return log("Please specify a player id!")
+         }
+         var player = main.getPlayer(id)
+         if (!player) {
+             return log("That player doesnt exsist!")
+         }
+         var amount = parseInt(str[2])
+        
+         if (isNaN(amount) && player.minions.length != 0) {
+           
+        for (var i = 0; i < player.minions.length; i ++) {
+                 main.removeMinion(player.minions[i])
+                 i--;
+             }
+             return log("Removed minions for " + player.gameData.name) 
+         } else {
+            if (isNaN(amount)) amount = 1;
+             main.addMinions(player,amount)
+             return log("Gave " + player.gameData.name + " " + amount + " minions")
+         }
+     }
