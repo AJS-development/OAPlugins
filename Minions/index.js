3269a62b7bcbf3188bfe3591d90651662cad712a
Andrew S <andrews54757@gmail.com>
2016-11-23 14:43:27 -0500

    Update index.js

--- Minions/index.js
+++ Minions/index.js
@@ -7,38 +7,7 @@ minVersion: '3.0.5', // minimum version requirements (optional)
 version:  '1.0.0', // version REQUIRED
 addToHelp: ["| Minion                   | Minion command           |"], // add to help
  commands: {
-     minion: function(str,main,log) {
-         str = str.split(" ")
-         if (str[1] == "destroy") {
-             main.minions.forEach((minion)=>{
-                 main.removeMinion(minion)
-             })
-             
-             return log("Removed all minions")
-         }
-             var id = parseInt(str[1])
-         if (isNaN(id)) {
-             return log("Please specify a player id!")
-         }
-         var player = main.getPlayer(id)
-         if (!player) {
-             return log("That player doesnt exsist!")
-         }
-         var amount = parseInt(str[2])
-        
-         if (isNaN(amount) && player.minions.length != 0) {
-           
-        for (var i = 0; i < player.minions.length; i ++) {
-                 main.removeMinion(player.minions[i])
-                 i--;
-             }
-             return log("Removed minions for " + player.gameData.name) 
-         } else {
-            if (isNaN(amount)) amount = 1;
-             main.addMinions(player,amount)
-             return log("Gave " + player.gameData.name + " " + amount + " minions")
-         }
-     }
+     minion: require('./minion.js')
      
  }, // commands
  configFile: false, //config file
