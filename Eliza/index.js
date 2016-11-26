328132939d95e0c0df8d2fe0801b77f5eab42919
Andrew S <andrews54757@gmail.com>
2016-11-26 13:58:53 -0500

    Update index.js

--- Eliza/index.js
+++ Eliza/index.js
@@ -5,7 +5,7 @@ name: "Eliza", // Name of plugin REQUIRED
 author: "Andrews54757", // author REQUIRED
 description: 'The E.L.I.Z.A chat bot', // Desciprtion
 minVersion: '', // minimum version requirements (optional)
-version:  '', // version REQUIRED
+version:  '1.0.0', // version REQUIRED
 addToHelp: false, // add to help
  commands: false, // commands
  configFile: false, //config file
