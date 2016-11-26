e980cb8b75a71ef13da7bb2fb6bd671014f41d03
Andrew S <andrews54757@gmail.com>
2016-11-26 13:58:22 -0500

    Update index.js

--- Eliza/index.js
+++ Eliza/index.js
@@ -1,1 +1,16 @@
 
+module.exports = {
+// [General]
+name: "Eliza", // Name of plugin REQUIRED
+author: "Andrews54757", // author REQUIRED
+description: 'The E.L.I.Z.A chat bot', // Desciprtion
+minVersion: '', // minimum version requirements (optional)
+version:  '', // version REQUIRED
+addToHelp: false, // add to help
+ commands: false, // commands
+ configFile: false, //config file
+// [Functions]
+init: function (data,configs) {
+  // init, Used to do stuff such as overriding things
+}
+}
