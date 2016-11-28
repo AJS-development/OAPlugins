563e9e6bf39038b4a755a9903f662d580c968ac6
Andrew S <andrews54757@gmail.com>
2016-11-28 16:25:51 -0500

    Update index.js

--- dev/index.js
+++ dev/index.js
@@ -1,1 +1,19 @@
 
+module.exports = {
+// [General]
+name: "Dev", // Name of plugin REQUIRED
+author: "Andrews54757", // author REQUIRED
+description: 'Development plugin', // Desciprtion
+minVersion: '', // minimum version requirements (optional)
+version:  '1.0.0', // version REQUIRED
+addToHelp: false, // add to help
+ commands: {
+     comp: require('./compile.js')
+     
+ }, // commands
+ configFile: false, //config file
+// [Functions]
+init: function (data,configs) {
+  // init, Used to do stuff such as overriding things
+}
+}
