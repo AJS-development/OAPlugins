df5fec50dc487e14e01736acefd54227673e9ab0
Andrew S <andrews54757@gmail.com>
2017-01-07 16:58:46 -0500

    Update index.js

--- Minions/index.js
+++ Minions/index.js
@@ -3,7 +3,7 @@ module.exports = {
 name: "Minions", // Name of plugin REQUIRED
 author: "Andrews54757", // author REQUIRED
 description: 'Adds minions to the game', // Desciprtion
-minVersion: '3.0.5', // minimum version requirements (optional)
+minVersion: '3.1.0', // minimum version requirements (optional)
 version:  '1.0.0', // version REQUIRED
 addToHelp: ["| Minion                   | Minion command           |"], // add to help
  commands: {
