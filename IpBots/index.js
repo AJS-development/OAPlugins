e20cc2a72fd9f04cc97d9b9aa1c3144228fef028
Andrew S <andrews54757@gmail.com>
2017-01-09 12:26:10 -0500

    Update index.js

--- IpBots/index.js
+++ IpBots/index.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+// [General]
+name: "IPBots", // Name of plugin REQUIRED
+author: "Andrews54757", // author REQUIRED
+description: 'Gives bots to a ip', // Desciprtion
+minVersion: '', // minimum version requirements (optional)
+version:  '1.0.0', // version REQUIRED
+addToHelp: false, // add to help
+ commands: false, // commands
+ configFile: "config.ini", //config file
+// [Functions]
+init: function (data,configs) {
+  // init, Used to do stuff such as overriding things
+}
+}
