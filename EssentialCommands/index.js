394829ed54c2b3065b8eb8de60eebe9fdd1972b8
ᦖєgιт <support@legitsoulja.info>
2017-04-23 12:14:24 -0400

    Update index.js

--- EssentialCommands/index.js
+++ EssentialCommands/index.js
@@ -5,7 +5,7 @@ name: "Essential Commands", // Name of plugin REQUIRED
 author: "Andrews54757", // author REQUIRED
 description: 'Essential commands such as mass or name', // Desciprtion
 minVersion: '', // minimum version requirements (optional)
-version:  '1.0.0', // version REQUIRED
+version:  '1.0.1', // version REQUIRED
 commands: {
     mass: require('./mass.js'),
     name: require('./name.js'),
@@ -30,7 +30,14 @@ commands: {
               "| Change                   | Change config value      |",
               "| Pm                       | Send a msg to a player   |",
               "| Prune                    | Prune a player's msg     |",
-              "| Kill                     | Kills a player           |"],
+              "| Kill                     | Kills a player           |",
+			  "| Clear                    | Clears Console           |",
+			  "| Freeze                   | Freeze Player            |",
+			  "| Unfreeze                 | Un-Freeze player         |",
+			  "| Config                   | Shows editable configs   |",
+			  "| Kill                     | Kills a player           |",
+			  "| Change                   | Change config value      |",
+			  ],
 // [Functions]
 init: function (data,config) {
   // init, Used to do stuff such as overriding things
