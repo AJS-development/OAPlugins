b9a4040ae3054d773216534b12e0755e578b8a9b
ᦖєgιт <support@legitsoulja.info>
2017-04-23 12:14:12 -0400

    Update config.js

--- EssentialCommands/config.js
+++ EssentialCommands/config.js
@@ -1,10 +1,13 @@
 module.exports = function(str,main,log) {
-	function genSpace(i,r = ""){
-		for(var o = 0; o < i; o++){r += " ";} return r;
+	var genSpace = function(e){
+		var r = "";
+		for(var i = 0; i < e; i++) r += " ";
+		return r;
 	}
 	log("------------------------------------------------------------");
 	for(var i in main.dataService.config){
+		if(!main.dataService.config[i]) continue;
 		log(i + genSpace(25 - i.length) + "=" + genSpace(5) + main.dataService.config[i]);
 	}
-  log("------------------------------------------------------------");
+	log("------------------------------------------------------------");
 }
